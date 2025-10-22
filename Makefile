77dc9aeeae9218795f786db697fd3e720b3f8b66
j0shtran <hienjoshuatran@gmail.com>
2025-10-22 12:39:34 -0500

    second commit

--- Makefile
+++ Makefile
@@ -1,14 +1,13 @@
 #Football makefile
 CC = gcc
-CFLAGS = -c -Wall 
+CFLAGS = -Wall 
 
-all: prog 
-
-prog: lab8_1.o 
-	$(CC) lab8_1.o -o prog 
-lab8_1.o: lab8_1.c
-	$(CC) $(CFLAGS) lab8_1.c 
-
+all: lab8_1 lab8_2
+ 
+lab8_1: lab8_1.c
+	$(CC) $(CFLAGS) lab8_1.c -o lab8_1 
+lab8_2: lab8_2.c 
+	$(CC) $(CFLAGS) lab8_2.c -o lab8_2 
 clean:
-	rm -rf *.o 
+	rm -rf *.o -f lab8_1 lab8_2 
 
--- lab8_2.c
+++ lab8_2.c
@@ -2,50 +2,50 @@
 int main() {
   float init_temp, conv_temp;
   printf("Enter the temperature: ");
-  scanf("%d", &temp);
+  scanf("%f", &init_temp);
   char init_val, conv_val;
   printf("Enter the original scale (C, F, K): ");
-  scanf("%d", &init_val);
+  scanf("%c", &init_val);
   printf("Enter the conversion scale (C, F, K): ");
-  scanf("%d", &conv_val);
-  if (init_val == "C") {
-    if (conv_val == "F") {
+  scanf("%c", &conv_val);
+  if (init_val == 'C') {
+    if (conv_val == 'F') {
       conv_temp = (init_temp * (9/5)) + 32;
-      printf("Converted temperature: %d", conv_temp);
+      printf("Converted temperature: %f", conv_temp);
     }
-    elif (conv_val == "K") {
+    else if (conv_val == 'K') {
       conv_temp = init_temp + 273.15;
-      printf("Converted temperature: %d", conv_temp);
+      printf("Converted temperature: %f", conv_temp);
     }
-    elif (conv_val == "C") {
-        printf("Converted temperature: %d", init_temp);
+    else if (conv_val == 'C') {
+        printf("Converted temperature: %f", init_temp);
       }
-  }
-  elif (init_val == "F") {
-    if (conv_val == "C") {
+ }
+  else if (init_val == 'F') {
+    if (conv_val == 'C') {
      conv_temp = (init_temp - 32) * (5/9); 
-     printf("Converted temperature: %d", conv_temp);
+     printf("Converted temperature: %f", conv_temp);
     }
-    elif (conv_val == "K") {
+    else if (conv_val == 'K') {
       conv_temp = (init_temp +459.67) * (5/9);
-      printf("Converted temperature: %d", conv_temp);
+      printf("Converted temperature: %f", conv_temp);
       }        
-    elif (conv_val == "F") {
-      printf("Converted temperature: %d", init_temp);
+    else if (conv_val == 'F') {
+      printf("Converted temperature: %f", init_temp);
       }
   
   }
-  elif (init_val == "K") {
-    if (conv_val == "C") {
-      conv_temp = init_val - 273.15;
-      printf("Converted temperature: %d", conv_temp);
+  else if (init_val == 'K') {
+    if (conv_val == 'C') {
+      conv_temp = init_temp - 273.15;
+      printf("Converted temperature: %f", conv_temp);
     }
-    elif (conv_val == "F") {
-      conv_temp == (init_val * (9/5)) - 459.67
-      printf("Converted temperature: %d", conv_temp);
+    else if (conv_val == 'F') {
+      conv_temp = (init_temp * (9/5)) - 459.67;
+      printf("Converted temperature: %f", conv_temp);
     }
-    elif (conv_val == "K") {
-      printf("Converted temperature: %d", init_temp);
+    else if (conv_val == 'K') {
+      printf("Converted temperature: %f", init_temp);
     }
   }
 }
